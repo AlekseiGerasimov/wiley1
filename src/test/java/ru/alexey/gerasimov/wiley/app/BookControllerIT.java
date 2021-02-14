f9a86ccb5f84402686e7afa436f7712edcbc72e1
Alex <leschagerasimov@yandex.ru>
2021-02-14 16:13:16 +0300

    Remove unused imports

--- src/test/java/ru/alexey/gerasimov/wiley/app/BookControllerIT.java
+++ src/test/java/ru/alexey/gerasimov/wiley/app/BookControllerIT.java
@@ -11,10 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.alexey.gerasimov.wiley.app.repository.BookRepositoryStub;
-import ru.alexey.gerasimov.wiley.app.service.BookService;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
--- src/test/java/ru/alexey/gerasimov/wiley/app/BookControllerTest.java
+++ src/test/java/ru/alexey/gerasimov/wiley/app/BookControllerTest.java
@@ -7,19 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.alexey.gerasimov.wiley.app.controller.BookController;
 import ru.alexey.gerasimov.wiley.app.service.BookService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Tag("unit")
 @ExtendWith(MockitoExtension.class)
